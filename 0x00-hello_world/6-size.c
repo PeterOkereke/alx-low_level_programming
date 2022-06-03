572d192016083aa8c33a5aa54f476dd91bdbb137
Peter Okereke <thepeterokereke@gmail.com>
2022-06-02 18:41:55 -0700

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return - Always 0 (Success)
+ */
+int main(void)
+{
+	char charType;
+	int intType;
+	long longType;
+	long long longlongType;
+	float floatType;
+
+	printf("Size of a char: %Id byte(s)\n", sizeof(charType));
+	printf("Size of an int: %Id byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %Id byte(s)\n", sizeof(longType));
+	printf("Size of a long long int: %Id byte(s)\n", sizeof(longlongType));
+	printf("Size of a float: %Id byte(s)\n", sizeof(floatType));
+	return (0);
+}
