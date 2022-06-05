20ade42168d85458ec819ce41fe2904d0f6d599b
Peter Okereke <thepeterokereke@gmail.com>
2022-06-04 17:57:24 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the lowercase alphabet in reverse
+ * followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha = 'z';
+
+	while (alpha >= 'a')
+	{
+		putchar(alpha);
+		alpha--;
+	}
+	putchar('\n');
+	return (0);
+}
