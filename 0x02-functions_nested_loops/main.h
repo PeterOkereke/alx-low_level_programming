2ea027f90fef02c2fc4fae9ab00d49dadb9168dc
Peter Okereke <thepeterokereke@gmail.com>
2022-06-09 14:12:16 -0700

    Task 1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,4 +8,11 @@ int _putchar(char);
  * Return: Always 0 (Success)
  */
 
+void print_alphabet(void);
+/**
+ * print_alphabet - Prints letters in lowercase
+ * followed by a new line.
+ * Return: Always 0 (Success)
+ */
+
 #endif
