a193fac5d4c40554884f6fb8645c79f9045aa8c3
Peter Okereke <thepeterokereke@gmail.com>
2022-06-09 15:25:41 -0700

    Task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  * _islower - Checks for lowercase character
- * c: character to check for
+ * @c: character to check for.
  * Return: 1 if c is lowercase. Otherwise 0.
  */
 int _islower(int c)
