622624d4c17ed32f431b84d9836428028021b113
Peter Okereke <thepeterokereke@gmail.com>
2022-06-09 11:04:55 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Code block
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(x[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
