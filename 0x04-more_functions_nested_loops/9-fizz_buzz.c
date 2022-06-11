e3f68e1f6c3393d07ce34a63d414d6c1bac21fd6
Peter Okereke <thepeterokereke@gmail.com>
2022-06-11 12:53:25 -0700

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -20,12 +20,12 @@ int main(void)
 	{
 		if (num == 100)
 			printf("%s", b);
+		else if ((num % 3 == 0) && (num % 5 == 0)
+				printf("%s ", fb);
 		else if (num % 3 == 0)
 			printf("%s ", f);
 		else if (num % 5 == 0)
 			printf("%s ", b);
-		else if ((num % 3 == 0) && (num % 5 == 0))
-			printf("%s ", fb);
 		else
 			printf("%d ", num);
 	}
