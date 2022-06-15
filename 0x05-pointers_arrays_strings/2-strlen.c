7289cd5b6bbafdbeee3d64b52144430b35218820
Peter Okereke <thepeterokereke@gmail.com>
2022-06-14 17:44:33 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen - A function that returns the length of a string
+ * @s: char input
+ * Return: length of the string
+ */
+
+int _strlen(char *s)
+{
+	int i = 1, sum = 0;
+	char sl = s[0];
+
+	while (sl != '\0')
+	{
+		sum++;
+		char sl = s[i++];
+	}
+	return (sum);
+}
