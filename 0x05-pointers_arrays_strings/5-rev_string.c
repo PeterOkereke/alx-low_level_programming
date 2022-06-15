cac439976985eb0f2b288de69a83f3376fdd2981
Peter Okereke <thepeterokereke@gmail.com>
2022-06-14 18:34:23 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - A function that reverses a string
+ * @s: input to reverse
+ * Return: string in reverse
+ */
+
+void rev_string(char *s)
+{
+	char rev = s[0];
+	int scounter = 0;
+	int i;
+
+	while (s[scounter] != '\0')
+		scounter++;
+
+	for (i = 0; i < scounter; i++)
+	{
+		scounter--;
+		rev = s[i];
+		s[i] = s[scounter];
+		s[scounter] = rev;
+	}
+}
