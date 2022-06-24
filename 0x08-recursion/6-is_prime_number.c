d58fb735ccec0eda87c60836fbf79edaed815343
Peter Okereke <thepeterokereke@gmail.com>
2022-06-23 21:09:55 -0700

    Task 9

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -26,9 +26,9 @@ int is_prime_number(int n)
 {
 	if (n == 0)
 		return (0);
-	else if (n < 0)
+	if (n < 0)
 		return (-1);
-	else if (n == 1)
+	if (n == 1)
 		return (1);
 	return (is_prime(n, 2));
 }
