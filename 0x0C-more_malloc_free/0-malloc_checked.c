d92da8b2ef524b3498efa8009e5d2b0e1686c7fa
Peter Okereke <thepeterokereke@gmail.com>
2022-07-02 19:48:03 -0700

    Project commits

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory.
+ * @b: number of bytes.
+ *
+ * Return: pointer to the allocated memory.
+ * if malloc fails, status return value is 98.
+ */
+void *malloc_checked(unsigned int b)
+{
+	char *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * array_range - creates an array of integers.
+ * @min: minimum value.
+ * @max: maximum value.
+ *
+ * Return: pointer to the newly created array.
+ * if min > max, returns NULL.
+ * if malloc fails, return NULL.
+ */
+int *array_range(int min, int max)
+{
+	int *ar;
+	int n;
+
+	if (min > max)
+		return (NULL);
+
+	ar = malloc(sizeof(*ar) * ((max - min) + 1));
+
+	if (ar == NULL)
+		return (NULL);
+
+	for (n = 0; min <= max; n++, min++)
+		ar[n] = min;
+
+	return (ar);
+}
